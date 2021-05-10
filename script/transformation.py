c9cb4cae397615af2c1a146beebe2f79db94b471
OkamiRvS <okamirvs@gmail.com>
2021-05-10 10:07:11 +0200

    new points and normalization

--- script/transformation.py
+++ script/transformation.py
@@ -1,5 +1,42 @@
 from scipy.linalg import null_space
 import numpy as np
+import pdb
+import cv2
+
+def createGrid(pxstep=20):
+    img = cv2.imread('../src/WSC_sample.png', cv2.IMREAD_COLOR)
+    width, height, channels = img.shape
+
+    grid = np.zeros((width, height))
+    x = pxstep
+    y = pxstep
+
+    #Draw all x lines
+    while x < img.shape[1]:
+        cv2.line(grid, (x, 0), (x, width), color=(255, 0, 255), thickness=1)
+        x += pxstep
+    
+    while y < img.shape[0]:
+        cv2.line(grid, (0, y), (height, y), color=(255, 0, 255),thickness=1)
+        y += pxstep
+
+    pts1 = np.float32([[0, 0], [width, 0], [0,height], [width, height] ])
+    pts2 = np.float32([[100, 0], [width-100, 0], [0,height], [width, height] ])
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)
+    warped = cv2.warpPerspective(grid, matrix, (height, width))
+
+    pdb.set_trace()
+
+    # [blend_images]
+    alpha = 0.5
+    beta = (1.0 - alpha)
+    dst = cv2.addWeighted(img, alpha, warped, beta, 0.0)
+
+    cv2.imshow('dst', dst)
+    cv2.imshow("Warped", warped)
+    cv2.imshow('Hehe', grid)
+    cv2.waitKey(0)
+
 
 
 def compute(x_i, X_i):
@@ -20,17 +57,41 @@ x = np.array([
 ])
 
 X = np.array([
-    [-8.89, 1.7845, 0.05, 1],
-    [8.89, 1.7845, 0, 1],
-    [-8.89, 0, 0, 1],
-    [8.89, 0, 0, 1],
-    [-8.89, -1.7845, 0, 1],
-    [8.89, -1.7845, 0, 1],
+    [8.89, 1.7845, 0, 1], # top-right-inside corner
+    [-8.89, 1.7845, 0, 1], # top-left-inside corner
+    [-8.89, -1.7845, 0, 1], # bottom-left
+    [8.89, -1.7845, 0, 1], # bottom-right
+
+    [8.94, 1.7895, 0.035, 1], # top-right-outside corner
+    [-8.94, 1.7895, 0.035, 1], # top-left-outside corner
+    [-8.94, -1.7895, 0.035, 1], # bottom-left-outside corner
+    [8.94, -1.7895, 0.035, 1], # bottom-right-outside corner
+
+    [0, 0, 0, 1], # blue ball
+    [0, -0.89225, 0, 1], # pink ball
+    [0, -1.4605, 0, 1], # black ball
+    [0, 1.0475, 0, 1], # brown ball
+    [0.292, 1.0475, 0, 1], # green ball
+    [-0.292, 1.0475, 0, 1], # yellow ball
+
 ])
+
+def normalize(v):
+    norm = np.linalg.norm(v)
+    if norm == 0: 
+       return v
+    return v / norm
 
 A = None
 for x_i, X_i in zip(x, X):
     A = compute(x_i, X_i) if A is None else np.concatenate((A, compute(x_i, X_i)))
+    
+#createGrid()
+
+rank = np.linalg.matrix_rank(A) 
+pdb.set_trace()
 
 p = null_space(A)
+p = normalize(p)
 p = p.reshape((3, 4))
+
