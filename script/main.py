886fed6222eb4d23a69cfe2939b0a5bb9d2aab19
Umberto <okamirvs@gmail.com>
2021-05-12 19:05:24 +0200

    edit video_frame_read script

--- script/main.py
+++ script/main.py
@@ -2,19 +2,24 @@ from Find_angles import Find_angles
 from Average_rgb import Average_rgb
 from Hough_circles import Hough_circles
 from Transformation import Transformation
-
+from Video_frame_read import Video_frame_read
 
 def main():
+    # First task
     imgpath = "src/WSC_sample_good.png"
     #find_angles(imgpath).run()
 
     img_maskpath = "src/WSC_mask.png"
+    #points = Hough_circles(imgpath, img_maskpath).run()
+
+    #Transformation(imgpath, points).run()
+
+    # Second task
     #average_rgb(imgpath, img_maskpath).run()
 
-    points = Hough_circles(imgpath, img_maskpath).run()
-
-    Transformation(imgpath, points).run()
-
+    videopath = "src/WSC.mp4"
+    Video_frame_read(videopath, img_maskpath).run()
+    
 
 if __name__ == "__main__":
     main()
--- script/video_frame_read.py
+++ script/video_frame_read.py
@@ -5,44 +5,53 @@ from pathlib import Path
 from tqdm import tqdm
 
 
-filename = 'src/WSC_trimmed_video.mp4'
-cap = cv2.VideoCapture(filename)
+class Video_frame_read():
 
-mask = cv2.imread('src/WSC_mask.png', 0)
-n_el = np.sum(mask == 255)
+    def __init__(self, videopath, img_maskpath):
+        self.videopath = videopath
+        self.img_maskpath = img_maskpath
 
-r, g, b = [], [], []
+    def run(self):
+        self.read_video()
 
-Path("images/").mkdir(parents=True, exist_ok=True)
+    def read_video(self):
+        cap = cv2.VideoCapture(self.videopath)
 
-if cap.isOpened():
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = cap.get(cv2.CAP_PROP_FPS)
+        mask = cv2.imread(self.img_maskpath, 0)
+        n_el = np.sum(mask == 255)
 
-    fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+        r, g, b = [], [], []
 
-    it = 0
-    pbar = tqdm(total=int(cap.get(cv2.CAP_PROP_FRAME_COUNT)))
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        it += 1
-        pbar.update(1)
+        Path("images/").mkdir(parents=True, exist_ok=True)
 
-        res = cv2.bitwise_and(frame, frame, mask=mask)
+        if cap.isOpened():
+            width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            fps = cap.get(cv2.CAP_PROP_FPS)
 
-        r.append(np.sum(res[:, :, 2] * (mask == 255)) / n_el)
-        g.append(np.sum(res[:, :, 1] * (mask == 255)) / n_el)
-        b.append(np.sum(res[:, :, 0] * (mask == 255)) / n_el)
+            fourcc = cv2.VideoWriter_fourcc(*'DIVX')
 
-        if 115 <= g[-1] <= 135 and r[-1] < 15 and b[-1] < 25:
-            cv2.imwrite(f'images/{it}.png', res)
-    pbar.close()
-    cap.release()
+            it = 0
+            pbar = tqdm(total=int(cap.get(cv2.CAP_PROP_FRAME_COUNT)))
+            while True:
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                it += 1
+                pbar.update(1)
 
-plt.plot(r, color='red')
-plt.plot(g, color='green')
-plt.plot(b, color='blue')
-plt.show()
+                res = cv2.bitwise_and(frame, frame, mask=mask)
+
+                r.append(np.sum(res[:, :, 2] * (mask == 255)) / n_el)
+                g.append(np.sum(res[:, :, 1] * (mask == 255)) / n_el)
+                b.append(np.sum(res[:, :, 0] * (mask == 255)) / n_el)
+
+                if 115 <= g[-1] <= 135 and r[-1] < 15 and b[-1] < 25:
+                    cv2.imwrite(f'images/{it}.png', res)
+            pbar.close()
+            cap.release()
+
+        plt.plot(r, color='red')
+        plt.plot(g, color='green')
+        plt.plot(b, color='blue')
+        plt.show()
